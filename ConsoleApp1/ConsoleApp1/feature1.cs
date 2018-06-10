5a76870850a81cd35cba47a605d16912988fc491
shubham <agrawals1@rknec.edu>
2018-06-10 15:19:41 +0530

    feature 1 ADDED

---
+++ ConsoleApp1/ConsoleApp1/feature1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class feature1
+    {
+        public int add()
+        {
+            int x = 15;
+            int y = 20;
+            return (x + y);
+        }
+    }
+}
